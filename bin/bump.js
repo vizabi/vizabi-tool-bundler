cf4866c288fd7ea1a7c03fa5cdeaad7950d97aab
Angie the Fairytale <konzeptmeister@gmail.com>
2020-02-10 20:24:41 +0100

    Update version bump behavior

--- bin/bump.js
+++ bin/bump.js
@@ -7,27 +7,26 @@ const path = require('path');
 
 const projectDir = process.cwd();
 const pjson = require(path.join(projectDir, 'package.json'));
-console.log("--- Current package.json on git");
-const oldPjson = JSON.parse(shell.exec("git show HEAD~1:package.json").stdout);
+//console.log("--- Current package.json on git");
+//const oldPjson = JSON.parse(shell.exec("git show HEAD~1:package.json").stdout);
 const lastPublishedVersion = shell.exec("npm show " + pjson.name + " version").stdout.trim();
-const versionBump = cv(lastPublishedVersion, pjson.version) <= 0;
+const versionBump = cv(lastPublishedVersion, pjson.version) >= 0;
 
 const bump = () => {
   console.log("--- Latest published version: ", lastPublishedVersion);
-  console.log("--- Previous package.json version: ", oldPjson.version);
+  //console.log("--- Previous package.json version: ", oldPjson.version);
   console.log("--- Current package.json version: ", pjson.version);
   console.log("--- Need to bump version:", versionBump);
 
   if (versionBump) {
-    console.log("--- Performing version bump...");
-    //pjson.version = lastPublishedVersion;
-    //fs.writeFileSync(path.join(projectDir, 'package.json'), JSON.stringify(pjson, null, 2));
+    console.log("--- Performing version bump based on latest published version...");
+    pjson.version = lastPublishedVersion;
+    fs.writeFileSync(path.join(projectDir, 'package.json'), JSON.stringify(pjson, null, 2));
     shell.exec("npm version --no-git-tag-version patch");
-    shell.exec("git status");
     shell.exec("git add package.json");
     shell.exec("git config --global user.name travis");
     shell.exec("git config --global user.email travis@example.com");
-    shell.exec("git commit --amend -C $(git rev-parse --verify HEAD)");
+    console.log("git commit v" + require(path.join(projectDir, 'package.json')).version);
     // need rebuild elsewhere after version bump so that the published vizabi is self-aware of its version
   }
 };
