7a764773648bb398a922b6bed19e6a9bf5117526
Angie the Fairytale <konzeptmeister@gmail.com>
2020-02-10 19:50:52 +0100

    Update version bump behavior

--- bin/bump.js
+++ bin/bump.js
@@ -7,31 +7,28 @@ const path = require('path');
 
 const projectDir = process.cwd();
 const pjson = require(path.join(projectDir, 'package.json'));
+console.log("--- Current package.json on git");
 const oldPjson = JSON.parse(shell.exec("git show HEAD~1:package.json").stdout);
 const lastPublishedVersion = shell.exec("npm show " + pjson.name + " version").stdout.trim();
-const versionBump = cv(oldPjson.version, pjson.version) < 0;
+const versionBump = cv(lastPublishedVersion, pjson.version) <= 0;
 
 const bump = () => {
   console.log("--- Latest published version: ", lastPublishedVersion);
   console.log("--- Previous package.json version: ", oldPjson.version);
   console.log("--- Current package.json version: ", pjson.version);
+  console.log("--- Need to bump version:", versionBump);
 
-  console.log("===== Bumped version:", versionBump);
-
-  if (!versionBump) {
-    console.log("--- Publishing...");
-    pjson.version = lastPublishedVersion;
-    fs.writeFileSync(path.join(projectDir, 'package.json'), JSON.stringify(pjson, null, 2));
+  if (versionBump) {
+    console.log("--- Performing version bump...");
+    //pjson.version = lastPublishedVersion;
+    //fs.writeFileSync(path.join(projectDir, 'package.json'), JSON.stringify(pjson, null, 2));
+    shell.exec("npm version --no-git-tag-version patch");
+    shell.exec("git status");
     shell.exec("git add package.json");
     shell.exec("git config --global user.name travis");
     shell.exec("git config --global user.email travis@example.com");
     shell.exec("git commit --amend -C $(git rev-parse --verify HEAD)");
-    shell.exec("npm version --no-git-tag-version prerelease");
-    shell.exec("git status");
-    shell.exec("git add package.json");
-    shell.exec("git commit --amend -C $(git rev-parse --verify HEAD)");
-    // need rebuild after version bump so that the published vizabi is self-aware of its version
-    //shell.exec("npm run build");
+    // need rebuild elsewhere after version bump so that the published vizabi is self-aware of its version
   }
 };
 
